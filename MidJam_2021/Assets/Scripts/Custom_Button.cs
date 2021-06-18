309feeb201bf96abd1c315fb613fdf64da369c94
Rasbpy <unterbrink.ryan@gmail.com>
2021-06-18 12:57:45 -0400

    added button shake

---
+++ MidJam_2021/Assets/Scripts/Custom_Button.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Custom_Button : MonoBehaviour
+{
+    protected Text buttonText;
+    [SerializeField] bool customShape = false;
+
+	protected virtual void Awake()
+	{
+        if (customShape)
+        {
+            GetComponent<Image>().alphaHitTestMinimumThreshold = 0.25f;
+        }
+
+        buttonText = transform.GetChild(0).GetComponent<Text>();
+	}
+
+    protected virtual void OnMouseOver()
+    {
+
+    }
+    protected virtual void OnMouseEnter()
+    {
+
+    }
+    protected virtual void OnMouseExit()
+    {
+
+    }
+    protected virtual void OnMouseDown()
+    {
+
+    }
+    protected virtual void OnMouseUp()
+    {
+
+    }
+    protected virtual void OnMouseUpAsButton()
+    {
+
+    }
+    protected virtual void OnMouseDrag()
+    {
+
+    }
+}
