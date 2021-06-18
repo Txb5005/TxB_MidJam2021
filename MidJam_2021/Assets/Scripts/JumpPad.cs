31b183676e8ad7a2369e948c6b86fac1260e4dd5
NickCrespo <nicholascrespo16@yahoo.com>
2021-06-18 15:24:58 -0700

    adding and edits
    adding sfx(gumdrop, jumpad, win, and splash)
    add prefabs jumppad, slow hazard, win trigger
    adding win trigger script(when player enters trigger will play sound/Need to add win screen)
    add jump pad script( when player enters will force them to go up)
    add slow hazard(when player enters will slow speed down)
    edits to speed boost script(added sound), slow down script(added sound)
    edit gum drop prefab(added audio source)
    edit metric(added house, pad, slow and win)

--- MidJam_2021/Assets/Scripts/JumpPad.cs
+++ MidJam_2021/Assets/Scripts/JumpPad.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlowDownTrigger : MonoBehaviour
+public class JumpPad : MonoBehaviour
 {
+    [Header("Stats")]
+    public float jumpForce;
+
     [Header("Assigned")]
     public PlayerCharacter player;
     public AudioSource audioSource;
 
     public void Start()
     {
+        // At very start find and assign the objects
         player = FindObjectOfType<PlayerCharacter>();
     }
-
     public void OnTriggerEnter(Collider other)
     {
-        if(other.GetComponent<PlayerCharacter>())
+        if (other.GetComponent<PlayerCharacter>())
         {
-            player.DecreaseSpeed();
+            player.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
             audioSource.Play();
         }
     }
