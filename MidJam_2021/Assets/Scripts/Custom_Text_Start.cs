29f491a77aa39949d4bf22e65e68646c260ce001
Rasbpy <unterbrink.ryan@gmail.com>
2021-06-19 14:38:38 -0500

    added title text swiggle

---
+++ MidJam_2021/Assets/Scripts/Custom_Text_Start.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Custom_Text_Start : MonoBehaviour
+{
+    Text text;
+    Timer timer;
+    [SerializeField] float minShakeAngle;
+    [SerializeField] float maxShakeAngle;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        timer = new Timer("shakeTimer", true, .5f, Shake, true);
+    }
+    void Update()
+    {
+        timer.Run();
+    }
+    void Shake()
+    {
+        //if (text.transform.rotation.z > 0)
+        //{
+        //    RotateText(-5);
+        //}
+        //else
+        //{
+        //    RotateText(5);
+        //}
+        float angle = Random.Range(minShakeAngle, maxShakeAngle);
+        if (text.transform.rotation.z > 0)
+        {
+            angle = -Mathf.Abs(angle);
+        }
+        else
+        {
+            angle = Mathf.Abs(angle);
+        }
+        RotateText(angle);
+    }
+    void RotateText(float zAngle)
+    {
+        text.transform.localRotation = Quaternion.Euler(0, 0, zAngle);
+    }
+}
