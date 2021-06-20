e37b9791a08b6cebf4d011e017849a5c545165f2
Rasbpy <unterbrink.ryan@gmail.com>
2021-06-19 22:00:21 -0400

    adding to boss hand
    adding cookie particle effect and screams for dead gingerbread men

--- MidJam_2021/Assets/Scripts/BossHand.cs
+++ MidJam_2021/Assets/Scripts/BossHand.cs
@@ -8,31 +8,40 @@ public class BossHand : MonoBehaviour
     Rigidbody rb;
     Camera cam;
     PlayerCharacter player;
+    List<AudioSource> gingerbreadMenAudioSources = new List<AudioSource>();
+    [SerializeField] List<AudioClip> gingerbreadMenAudioClips = new List<AudioClip>();
+    ParticleSystem cookieParticleSystem;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        cam = GameObject.FindObjectOfType<Camera>();
-        player = GameObject.FindObjectOfType<PlayerCharacter>();
+        cam = FindObjectOfType<Camera>();
+        player = FindObjectOfType<PlayerCharacter>();
+        Transform t = GameObject.Find("Gingerbread Men Audio").transform;
+        for (int i = 0; i < t.childCount; i++)
+        {
+            AudioSource ass = t.GetChild(i).GetComponent<AudioSource>();
+            if (ass)
+            {
+                gingerbreadMenAudioSources.Add(ass);
+            }
+        }
+        cookieParticleSystem = GameObject.Find("Cookie Particle Effect").GetComponent<ParticleSystem>();
         currentSpeed = player.maxSpeed - .5f;
     }
-
-
     void Update()
     {
         // Boss always moving forward
         rb.velocity = new Vector3(currentSpeed, rb.velocity.y, rb.velocity.z);
     }
-
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<AI_GingerBread>())
         {
-            other.gameObject.GetComponent<AI_GingerBread>().die();  // Kill Gingerbread
+            other.gameObject.GetComponent<AI_GingerBread>().die(); // Kill Gingerbread
             // Possible Animation (Hand grabbing gingerbread)
-            // Sound FX (Crunch)
-            // Particle FX (Crumbs/Pieces of gingerbread flying)
+            GingerbreadManScream();
+            cookieParticleSystem.Play();
             StartCoroutine(cam.Shake(.3f, .15f)); // Camera Shake function
             currentSpeed += .1f;  // Potential increase of boss speed and player max speed
             player.maxSpeed += .1f;
@@ -46,6 +55,20 @@ public class BossHand : MonoBehaviour
             if (true) // after death animation is fully complete
             {
                 Scene_Manager.Instance.LoadScene("Lose");
+            }
+        }
+    }
+
+    void GingerbreadManScream()
+    {
+        int rand = Random.Range(0, gingerbreadMenAudioClips.Count);
+        foreach (var item in gingerbreadMenAudioSources)
+        {
+            if (!item.isPlaying)
+            {
+                item.clip = gingerbreadMenAudioClips[rand];
+                item.Play();
+                return;
             }
         }
     }
